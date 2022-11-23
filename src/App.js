eec6f3fbe8451ba8be5719da7cbda507c0565701
ozcan cetin <cetin445@gmail.com>
2022-11-23 11:35:42 +0300

    test

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import { useSelector } from 'react-redux';
 import './App.css';
 import { Counter } from './features/counter/Counter';
 import Modal from './features/modal/Modal';
-import ReactGA from 'react-ga';
+// import ReactGA from 'react-ga';
 
-function initialGA(){
-  const TRACKING_ID = "G-WXGD3560YJ"; // OUR_TRACKING_ID
-  ReactGA.initialize(TRACKING_ID);
-  ReactGA.pageview("home");
-}
+// function initialGA(){
+//   const TRACKING_ID = "G-WXGD3560YJ"; // OUR_TRACKING_ID
+//   ReactGA.initialize(TRACKING_ID);
+//   ReactGA.pageview("home");
+// }
 
 function App() {
-  initialGA()
+  // initialGA()
   const { isOpen } = useSelector((state) => state.modal);
   console.log(isOpen)
 
