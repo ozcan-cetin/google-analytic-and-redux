b0c3dabc8962297a8035dfe05d7caddc02789d7b
ozcan cetin <cetin445@gmail.com>
2022-11-23 11:12:51 +0300

    test ga

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import { Counter } from './features/counter/Counter';
 import Modal from './features/modal/Modal';
 import ReactGA from 'react-ga';
+
+function initialGA(){
   const TRACKING_ID = "G-WXGD3560YJ"; // OUR_TRACKING_ID
   ReactGA.initialize(TRACKING_ID);
   ReactGA.pageview("home");
-
+}
 
 function App() {
+  initialGA()
   const { isOpen } = useSelector((state) => state.modal);
   console.log(isOpen)
 
